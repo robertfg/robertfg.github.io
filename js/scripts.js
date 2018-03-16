ece9d43ca97dc2088e9524d9b499c9c62bac05b9
Robert Glover <robert_f_g@hotmail.com>
2018-03-15 22:56:37 -0400

    Responsive Home Page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
+/* =================================
+   Functions
+==================================== */
+
+
+/* =================================
+   Handle "hamburger" events
+==================================== */
+
+// When you hover over the "hamburger," change the entire color.
+const hamburger_menu = document.getElementById('hamburger_menu');
+hamburger_menu.addEventListener('mouseover', (e) => {
+  const hamburger = document.getElementsByClassName('hamburger');
+  for ( let i = 0; i < hamburger.length; i++ ) {
+    hamburger[i].style.background = '#FDB47D';
+  }
+});
+
+// Change it back when you stop hovering
+hamburger_menu.addEventListener('mouseout', (e) => {
+  const hamburger = document.getElementsByClassName('hamburger');
+  for ( let i = 0; i < hamburger.length; i++ ) {
+    hamburger[i].style.backgroundColor = 'FloralWhite';
+  }
+});
+
 // Auto-update the year in the footer.
 let thisYear = document.getElementById('thisYear');
 thisYear.textContent = new Date().getFullYear();
