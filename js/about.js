a9a69abf835f3a1b810e3d72a921cbfcc4eea1a4
Robert Glover <robert_f_g@hotmail.com>
2018-03-27 20:39:06 -0400

    Adds Skills widget

---
+++ js/about.js
@@ -0,0 +1,39 @@
+/* =====================================================
+     Put up a list of my skills on the web page.
+  ======================================================== */
+
+// 1. Create an XML HTTP Request object
+let xhr = new XMLHttpRequest();
+
+// 2.	Create a callback function: programming you want to run whille when server returns its response.
+xhr.onreadystatechange = () => {
+  if ( xhr.readyState === 4 ) {
+    let skills = JSON.parse(xhr.responseText);
+
+    // Get the ul tag
+    let statusHTML = '<ul>';
+
+    // Loop for employees
+    for ( let i = 0; i < skills.length; i++ ) {
+      if ( skills[i].level === "beginner" ) {
+        statusHTML += '<li>';
+      } else if ( skills[i].level === "intermediate" ) {
+        statusHTML += '<li>';
+      } else {
+        statusHTML += '<li>';
+      }
+      statusHTML += skills[i].skill;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+
+    // Write to the page
+    document.getElementById('skills').innerHTML = statusHTML;
+  }
+};
+
+// 3.	Open a Request:
+xhr.open('GET', '../data/skills.json');
+
+// 4.	Send the request
+xhr.send();
